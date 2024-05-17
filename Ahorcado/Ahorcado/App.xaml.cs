590511d1635aeb540da6c4181b4b954c14d4de0b
AleRodriguez <21301061550103@cetis155.edu.mx>
2024-05-17 15:11:29 -0600

    Juego_Ahorcado
    Aquí se encuentran los códigos del juego del ahorcado

---
+++ Ahorcado/Ahorcado/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AhorcdoTEAM1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Ahorcado/Ahorcado/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AhorcdoTEAM1
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
