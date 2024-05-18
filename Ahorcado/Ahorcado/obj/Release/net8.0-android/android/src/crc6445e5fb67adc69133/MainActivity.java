76d4268a8298ae6d796a54fd3b2821cf29bb68fe
AleRodriguez <21301061550103@cetis155.edu.mx>
2024-05-17 18:09:40 -0600

    Juego_Ahorcado
    Aquí está el código del ahorcado

---
+++ Ahorcado/Ahorcado/obj/Release/net8.0-android/android/src/crc6445e5fb67adc69133/MainActivity.java
@@ -0,0 +1,48 @@
+package crc6445e5fb67adc69133;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("AhorcdoTEAM1.MainActivity, AhorcdoTEAM1", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("AhorcdoTEAM1.MainActivity, AhorcdoTEAM1", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("AhorcdoTEAM1.MainActivity, AhorcdoTEAM1", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
